fa472ca14545956ddcffccedae1d9f1cccfe1aae
eesaasheikh <eesaasheikh3@gmail.com>
2022-12-01 18:44:59 +0000

    test

--- Lab_Safari/safari_queries.sql
+++ Lab_Safari/safari_queries.sql
@@ -6,3 +6,5 @@ ON enclosures.id = animals.enclosure_id WHERE enclosure_id =1;
 
 
 --The names of the staff working in a given enclosure
+SELECT assignments.staff_id FROM assignments inner join enclosures
+ON enclosures.id = assignments.enclosure_id WHERE enclosure_id =1;
