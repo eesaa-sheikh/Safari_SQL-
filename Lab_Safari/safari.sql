1fbc2fb2c8fe9fa7eec301de35f560489bf0f474
eesaasheikh <eesaasheikh3@gmail.com>
2022-12-01 16:10:22 +0000

    Rimm comit

--- Lab_Safari/safari.sql
+++ Lab_Safari/safari.sql
@@ -1,13 +1,14 @@
 -- Safari Setup goes here
 
 -- MAKE A DROP FUNCTION.§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
-DROP TABLE enclosures;
 
 DROP TABLE assignments;
 
 DROP TABLE staffs;
 
 DROP TABLE animals;
+
+DROP TABLE enclosures;
 
 -- This is is a test
 
@@ -22,7 +23,6 @@ capacity INT,
 -- Employee ID
 employee_id INT REFERENCES employee(id),
 -- enclosure ID
-enclosure_id INT REFERENCES enclosure(id),
 closedForMaintenance BOOLEAN
 );
 
@@ -30,15 +30,17 @@ closedForMaintenance BOOLEAN
 CREATE TABLE staffs(
 id SERIAL PRIMARY KEY,
 name VARCHAR(255),
-employee number INT
+employeeNumber INT,
+enclosure_id INT REFERENCES enclosure(id)
 );
+
 
 --Assignment
 CREATE TABLE assignments(
 id SERIAL PRIMARY KEY,
-name VARCHAR(255),
-age INT,
-employee_id INT REFERENCES employee(id)
+day VARCHAR(255),
+employee_id INT REFERENCES employee(id),
+enclosure_id INT REFERENCES enclosure(id)
 );
 
 --animals
@@ -46,8 +48,56 @@ CREATE TABLE animals(
 id SERIAL PRIMARY KEY,
 name VARCHAR(255),
 age INT,
-enclosure_id INT REFERENCES enclosure(id),
+enclosure_id INT REFERENCES enclosure(id)
 );
+
+
+
+
+
+
+
+
+
+
+-- Employee (6 people)
+
+INSERT INTO staffs (name, employeeNumber, enclosure_id) VALUES ('Jonathon', 12780, 1);
+
+INSERT INTO staffs (name, employeeNumber, enclosure_id) VALUES ('Eesaa', 12102, 1);
+
+INSERT INTO staffs (name, employeeNumber, enclosure_id) VALUES ('Rimm',90812, 1);
+
+INSERT INTO staffs (name, employeeNumber, enclosure_id) VALUES ('Eon',89081, 1);
+
+INSERT INTO staffs (name, employeeNumber, enclosure_id) VALUES ('Ian',45670, 1);
+
+INSERT INTO staffs (name, employeeNumber, enclosure_id) VALUES ('Wilson',12031, 1);
+
+
+-- Animal (4 lions)
+
+INSERT INTO animals (name, age ,enclosure_id) VALUES ('LIL Meow Meow',5, 1);
+
+INSERT INTO animals (name, age ,enclosure_id) VALUES ('Meowth',1, 1);
+
+INSERT INTO animals (name, age ,enclosure_id) VALUES ('Prince',2, 1);
+
+INSERT INTO animals (name, age ,enclosure_id) VALUES ('Kebab',30, 1);
+
+
+-- enclosure 
+INSERT INTO enclosures (name, employee_id, capacity, closedForMaintenance) VALUES ('Lion', 1, 10, false);
+
+
+-- assignment
+INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (1, 1, 'Monday');
+INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (2, 1, 'Tuesday');
+INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (3, 1, 'Wednesday');
+INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (4, 1, 'Thursday');
+INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (5, 1, 'Friday');
+INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (6, 1, 'Saturday');
+
 
 
 
