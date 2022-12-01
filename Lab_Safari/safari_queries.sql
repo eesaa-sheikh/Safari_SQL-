9237621cab97a989a9c08400f9b18670700d73ae
eesaasheikh <eesaasheikh3@gmail.com>
2022-12-01 19:57:57 +0000

    Recent query update

--- Lab_Safari/safari_queries.sql
+++ Lab_Safari/safari_queries.sql
@@ -8,3 +8,12 @@ ON enclosures.id = animals.enclosure_id WHERE enclosure_id =1;
 --The names of the staff working in a given enclosure
 SELECT assignments.staff_id FROM assignments inner join enclosures
 ON enclosures.id = assignments.enclosure_id WHERE enclosure_id =1;
+
+-- Revised answer to the second questions
+SELECT staffs.name from staffs INNER JOIN assignments ON staffs.id = assignments.staff_id
+INNER JOIN enclosures ON enclosures.id = assignments.enclosures_id
+WHERE enclosures_id =1;
+
+
+
+--This is the current progress
