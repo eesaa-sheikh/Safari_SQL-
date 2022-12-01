11ae8a9fe449a715782918eb822f0fd9793d6c8b
eesaasheikh <eesaasheikh3@gmail.com>
2022-12-01 14:49:00 +0000

    Second commit

--- Lab_Safari/safari.sql
+++ Lab_Safari/safari.sql
@@ -1,22 +1,45 @@
 -- Safari Setup goes here
 
--- MAKE A DROP FUNCTION.
+-- MAKE A DROP FUNCTION.§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+DROP TABLE assignment;
 DROP TABLE animals;
 DROP TABLE enclosure;
+DROP TABLE staff;
+
 
 -- This is is a test
 
 
--- SETUP EACH TABLE.
+-- SETUP EACH TABLE.§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
+
+--animals
 CREATE TABLE animals(
 id SERIAL PRIMARY KEY,
 name VARCHAR(255),
 age INT
 );
 
+--Staff
+CREATE TABLE Staff(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+employee number INT
+);
+
+--Assignment
+CREATE TABLE Assignment(
+id SERIAL PRIMARY KEY,
+name VARCHAR(255),
+age INT
+);
+
+--enclosure
 CREATE TABLE enclosure(
 id SERIAL PRIMARY KEY,
 name VARCHAR(255),
 capacity INT,
 closedForMaintenance VARCHAR(255),
+
 );
+
+
